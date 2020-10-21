01861c548668437b47731f8cbf49f50d4e043b88
joelzhong <luckily.zhong@gmail.com>
2020-10-21 11:52:37 +0800

    add fluentd.conf

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # srcmesh-homework-blog
+
+
+[mysql and wordpress with persistent-volume](https://kubernetes.io/docs/tutorials/stateful-application/mysql-wordpress-persistent-volume/)
+
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,21 @@
 import { Construct } from 'constructs';
 import * as cdk8s from 'cdk8s';
-import * as k8s from 'cdk8s-plus';
+import * as kplus from 'cdk8s-plus';
 
 export class Homework extends cdk8s.Chart {
   constructor(scope: Construct, name: string) {
     super(scope, name);
 
     // Defines nginx config
-    const conf = new k8s.ConfigMap(this, 'NginxConf', {
+    const nginxConf = new kplus.ConfigMap(this, 'NginxConf', {
       metadata: {
         name: 'nginx-conf',
       },
     });
-    conf.addFile(`${__dirname}/asset/default.conf`, 'default.conf');
+    nginxConf.addFile(`${__dirname}/asset/nginx.conf`, 'default.conf');
     
-    // Defines config volume
-    const configVolume = k8s.Volume.fromConfigMap(conf, {
+    // Defines nginx config volume
+    const nginxConfigVolume = kplus.Volume.fromConfigMap(nginxConf, {
       name: `nginx-conf`,
       items: {
         'default.conf': {
@@ -24,11 +24,30 @@ export class Homework extends cdk8s.Chart {
       },
     });
     
+    // Defines fluentd config
+    const fluentdConf = new kplus.ConfigMap(this, 'FluentdConf', {
+      metadata: {
+        name: 'fluentd-conf'
+      },
+    });
+    fluentdConf.addFile(`${__dirname}/asset/fluentd.conf`, 'fluent.conf');
+
+    // Defines fluentd config volume
+    const fluentdConfingVolume = kplus.Volume.fromConfigMap(fluentdConf, {
+      name: `fluentd-conf`,
+      items: {
+        'fluent.conf': {
+          path: 'fluent.conf',
+        },
+      },
+    });
+
+
     // Defines log volume
-    const logVolume = k8s.Volume.fromEmptyDir('log');
+    const logVolume = kplus.Volume.fromEmptyDir('log');
 
     // Defines database secret
-    const dbSecret = new k8s.Secret(this, 'DbSecret', {
+    const dbSecret = new kplus.Secret(this, 'DbSecret', {
       metadata: {
         name: 'db-secret',
       },
@@ -39,27 +58,26 @@ export class Homework extends cdk8s.Chart {
         MYSQL_ROOT_PASSWORD: 'wqpejmo3n cpmo248',
       },
     });
-
     
     // Setup database
-    const db = new k8s.Service(this, 'DatabaseService', {
+    const db = new kplus.Service(this, 'DatabaseService', {
       metadata: {
         name: 'mysql-service',
       },
-      type: k8s.ServiceType.CLUSTER_IP,
+      type: kplus.ServiceType.CLUSTER_IP,
     });
-    db.addDeployment(new k8s.Deployment(this, 'Database', {
+    db.addDeployment(new kplus.Deployment(this, 'Database', {
       metadata: {
         name: 'mysql-deployment'
       },
       containers: [
-        new k8s.Container({
+        new kplus.Container({
           image: 'mysql:5.7.31',
           env: {
-            MYSQL_DATABASE: k8s.EnvValue.fromSecret(dbSecret, 'MYSQL_DATABASE'),
-            MYSQL_USER: k8s.EnvValue.fromSecret(dbSecret, 'MYSQL_USER'),
-            MYSQL_PASSWORD: k8s.EnvValue.fromSecret(dbSecret, 'MYSQL_PASSWORD'),
-            MYSQL_ROOT_PASSWORD: k8s.EnvValue.fromSecret(dbSecret, 'MYSQL_ROOT_PASSWORD'),
+            MYSQL_DATABASE: kplus.EnvValue.fromSecret(dbSecret, 'MYSQL_DATABASE'),
+            MYSQL_USER: kplus.EnvValue.fromSecret(dbSecret, 'MYSQL_USER'),
+            MYSQL_PASSWORD: kplus.EnvValue.fromSecret(dbSecret, 'MYSQL_PASSWORD'),
+            MYSQL_ROOT_PASSWORD: kplus.EnvValue.fromSecret(dbSecret, 'MYSQL_ROOT_PASSWORD'),
           },
         }),
       ],
@@ -67,48 +85,48 @@ export class Homework extends cdk8s.Chart {
     
 
     // Setup wordpress
-    const wordpress = new k8s.Service(this, 'WordpressService', {
+    const wordpress = new kplus.Service(this, 'WordpressService', {
       metadata: {
         name: 'wordpress-service',
       },
-      type: k8s.ServiceType.CLUSTER_IP,
+      type: kplus.ServiceType.CLUSTER_IP,
     });
-    wordpress.addDeployment(new k8s.Deployment(this, 'Wordpress', {
+    wordpress.addDeployment(new kplus.Deployment(this, 'Wordpress', {
       metadata: {
         name: 'wordpress-deployment'
       },
       containers: [
-        new k8s.Container({
+        new kplus.Container({
           image: 'wordpress:php7.2',
           env: {
-            WORDPRESS_DB_HOST: k8s.EnvValue.fromValue(db.name),
-            WORDPRESS_DB_USER: k8s.EnvValue.fromSecret(dbSecret, 'MYSQL_USER'),
-            WORDPRESS_DB_PASSWORD: k8s.EnvValue.fromSecret(dbSecret, 'MYSQL_PASSWORD'),
-            WORDPRESS_DB_NAME: k8s.EnvValue.fromSecret(dbSecret, 'MYSQL_DATABASE'),
+            WORDPRESS_DB_HOST: kplus.EnvValue.fromValue(db.name),
+            WORDPRESS_DB_USER: kplus.EnvValue.fromSecret(dbSecret, 'MYSQL_USER'),
+            WORDPRESS_DB_PASSWORD: kplus.EnvValue.fromSecret(dbSecret, 'MYSQL_PASSWORD'),
+            WORDPRESS_DB_NAME: kplus.EnvValue.fromSecret(dbSecret, 'MYSQL_DATABASE'),
           },
         }),
       ],
     }), 80);
 
     // Setup nginx
-    const nginx = new k8s.Service(this, 'NginxService', {
+    const nginx = new kplus.Service(this, 'NginxService', {
       metadata: {
         name: 'nginx-service'
       },
-      type: k8s.ServiceType.LOAD_BALANCER,
+      type: kplus.ServiceType.LOAD_BALANCER,
     });
-    nginx.addDeployment(new k8s.Deployment(this, 'Nginx', {
+    nginx.addDeployment(new kplus.Deployment(this, 'Nginx', {
       metadata: {
         name: 'nginx',
       },
       containers: [
-        new k8s.Container({
+        new kplus.Container({
           name: `main`,
           image: `nginx:stable`,
           volumeMounts: [
             {
               path: '/etc/nginx/conf.d',
-              volume: configVolume,
+              volume: nginxConfigVolume,
               readOnly: true,
             },
             {
@@ -117,19 +135,26 @@ export class Homework extends cdk8s.Chart {
             },
           ],
         }),
-        new k8s.Container({
+        new kplus.Container({
           name: `logger`,
           image: `fluent/fluentd:v1.9-1`,
+          volumeMounts: [
+            {
+              path: '/fluentd/etc',
+              volume: fluentdConfingVolume,
+              readOnly: true,
+            },
+          ]
         }),
       ],
     }), 80);
     
     // Setup ingress to nginx
-    new k8s.Ingress(this, 'Ingress', {
+    new kplus.Ingress(this, 'Ingress', {
       metadata: {
         name: 'ingress',
       },
-      defaultBackend: k8s.IngressBackend.fromService(nginx),
+      defaultBackend: kplus.IngressBackend.fromService(nginx),
     });
   }
 }
